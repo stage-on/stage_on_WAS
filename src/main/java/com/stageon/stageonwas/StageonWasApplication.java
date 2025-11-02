bf19c38120fa9a819e40ebc169a88a35603ef46a
DongWook-Kwak <kdw381@naver.com>
2025-11-02 16:53:19 +0900

    Initial commit

---
+++ src/main/java/com/stageon/stageonwas/StageonWasApplication.java
@@ -0,0 +1,13 @@
+package com.stageon.stageonwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageonWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageonWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stageon/stageonwas/StageonWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stageon.stageonwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageonWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
