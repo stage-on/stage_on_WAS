6d735726ab070568b38819a2a5ad486aea6bd436
DongWook-Kwak <kdw381@naver.com>
2025-11-05 23:59:18 +0900

    feat : 액세스 토큰 생성 및 검증 기능 구현

---
+++ src/main/java/com/stageon/stageonwas/domain/auth/jwt/JwtProvider.java
@@ -0,0 +1,68 @@
+package com.stageon.stageonwas.domain.auth.jwt;
+
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final SecretKey key;
+    private final long accessTokenExpiration;
+    private final String issuer;
+
+    public JwtProvider(
+            @Value("${jwt.secret-key}") String secretKey,
+            @Value("${jwt.expiration.access-token}") long accessTokenExpiration,
+            @Value("${jwt.issuer}") String issuer) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.accessTokenExpiration = accessTokenExpiration;
+        this.issuer = issuer;
+    }
+
+    public String generateAccessToken(String email) {
+        return createToken(email, accessTokenExpiration);
+    }
+
+    private String createToken(String subject, long expirationTime) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getSubject(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
