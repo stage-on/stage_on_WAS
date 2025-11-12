85c83c984cb53912470fafe42f2b08571f619c42
DongWook-Kwak <kdw381@naver.com>
2025-11-12 13:55:22 +0900

    feat : 불변성

--- src/main/java/com/stageon/stageonwas/domain/common/dto/ErrorResDto.java
+++ src/main/java/com/stageon/stageonwas/domain/common/dto/ErrorResDto.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public class ErrorResDto {
-    private String message;
-    private int status;
+    private final String message;
+    private final int status;
 
     public ErrorResDto(String message, int status) {
         this.message = message;
