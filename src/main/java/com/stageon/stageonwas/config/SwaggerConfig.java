cb3896c9ac004f82aa274d0ba67ea7086bac21bc
DongWook-Kwak <kdw381@naver.com>
2025-11-19 19:34:13 +0900

    docs : swagger settings

---
+++ src/main/java/com/stageon/stageonwas/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.stageon.stageonwas.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "STAGE-ON API", version = "v1", description = "API 명세"))
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
