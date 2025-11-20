bf8099efb5cf39ac09d15d71381a24dfc8b3a89a
DongWook-Kwak <kdw381@naver.com>
2025-11-20 19:26:45 +0900

    fix : userId 파라미터 제거 및 토큰 기반 인증적용

--- src/main/java/com/stageon/stageonwas/domain/allsearch/recommend/controller/RecommendController.java
+++ src/main/java/com/stageon/stageonwas/domain/allsearch/recommend/controller/RecommendController.java
@@ -3,8 +3,10 @@ package com.stageon.stageonwas.domain.allsearch.recommend.controller;
 
 import com.stageon.stageonwas.domain.allsearch.recommend.dto.RecommendedResDto;
 import com.stageon.stageonwas.domain.allsearch.recommend.service.RecommendService;
+import com.stageon.stageonwas.security.details.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,8 @@ public class RecommendController {
     private final RecommendService recommendService;
 
     @GetMapping
-    public ResponseEntity<List<RecommendedResDto>> getRecommendedKeywords() {
+    public ResponseEntity<List<RecommendedResDto>> getRecommendedKeywords(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
         List<RecommendedResDto> recommendedKeywords = recommendService.getRecommendedKeywords();
         return ResponseEntity.ok(recommendedKeywords);
     }
