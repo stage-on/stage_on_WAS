35f69f02d1291ba144d52429b1904351cef26f3e
DongWook-Kwak <kdw381@naver.com>
2025-11-20 21:22:28 +0900

    docs : 스웨거 주소 추가

--- src/main/java/com/stageon/stageonwas/security/config/OpenApiSecurityConfig.java
+++ src/main/java/com/stageon/stageonwas/security/config/OpenApiSecurityConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +16,8 @@ public class OpenApiSecurityConfig {
 
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-
+                .addServersItem(new Server().url("https://stage-on.duckdns.org").description("StageOn Server"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName, new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
