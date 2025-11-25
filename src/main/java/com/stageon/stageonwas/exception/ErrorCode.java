e3af849701c39bd74cd81eeecfd240806d51aea4
DongWook-Kwak <kdw381@naver.com>
2025-11-25 17:44:05 +0900

    fix : 좋아요 수 늘림

--- src/main/java/com/stageon/stageonwas/exception/ErrorCode.java
+++ src/main/java/com/stageon/stageonwas/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     PERFORMANCE_NOT_FOUND(404, "존재하지 않는 공연입니다."),
 
     // LIKE (좋아요 관련)
-    LIKE_LIMIT_EXCEEDED(400, "좋아요는 아티스트와 공연을 합쳐 최대 5개까지만 가능합니다."),
+    LIKE_LIMIT_EXCEEDED(400, "좋아요는 아티스트와 공연을 합쳐 최대 10개까지만 가능합니다."),
     LIKE_MIN_REQUIRED(400, "좋아요는 최소 2개를 유지해야 합니다."),
     ALREADY_LIKED(409, "이미 좋아요를 누른 대상입니다."),
     LIKE_NOT_FOUND(404, "좋아요 내역이 존재하지 않습니다."),
